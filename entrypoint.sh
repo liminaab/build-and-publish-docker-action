fb6f96bebf4ad122af739000d1ac81ded1c1ea75
furst <andreas.furst@gmail.com>
2020-06-10 16:16:49 +0200

    github sha

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,11 +20,13 @@ for repo in ${REPOS}; do
     # Do not do this for GO
     cp "${REPO_ROOT_DIR}/libs/captron_java_lib" . -r
 
-    TAG=$(echo $GITHUB_SHA | head -c7)
+    TAG=$(echo $GITHUB_SHA | cut -c7)
+    TAG2=$(echo $GITHUB_SHA | cut -c1-8)
     echo "${TAG} test"
+    echo "${TAG2} test2"
 
     # Change path
-    docker build -t "${REGISTRY}/liminaab/monorepo/${repo/apps\//}:3" .
+    docker build -t "${REGISTRY}/liminaab/monorepo/${repo/apps\//}:$(echo ${GITHUB_SHA} | cut -c1-8)" .
 
-    docker push "${REGISTRY}/liminaab/monorepo/${repo/apps\//}:3"
+    docker push "${REGISTRY}/liminaab/monorepo/${repo/apps\//}:$(echo ${GITHUB_SHA} | cut -c1-8)"
 done
